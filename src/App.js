0411a938763b5b60d4e80cdf3e9eaefe8f65b394
jakdev047 <jakfreelancer047@gmail.com>
2020-11-03 20:31:05 +0600

    node-sass fix

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// sass
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/sass/index.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h2>Hello World</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
